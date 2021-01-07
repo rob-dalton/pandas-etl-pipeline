28d2d8c72016e4c0b8a6a6fd347c16a59afefc59
Robert Dalton <robert@Roberts-MacBook-Pro.local>
2021-01-07 15:29:56 -0800

    Changed setup.py info from test to production.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pandas-pipeline-infinitely_improbable", # Replace with your own username
+    name="pandas-pipeline",
     version="0.1.0",
     author="Rob Dalton",
-    description="Package for creating data pipelines with Pandas DataFrames.",
+    description="Package for creating ETL pipelines with Pandas DataFrames.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rob-dalton/pandas-pipeline",
