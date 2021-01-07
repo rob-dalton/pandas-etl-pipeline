600803f1a4335949f8451a074c58031f5152f8a8
Robert Dalton <robert@Roberts-MacBook-Pro.local>
2021-01-07 15:41:52 -0800

    Renamed package.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Pandas Pipeline
+# Pandas ETL Pipeline
 This package contains the `Pipeline` class - a class for creating and running ETL pipelines using Pandas DataFrames.
 * [Documentation](https://github.com/rob-dalton/pandas-pipeline/wiki/Documentation)
 * [Examples](https://github.com/rob-dalton/pandas-pipeline/wiki/Examples).
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pandas-pipeline",
+    name="pandas-etl-pipeline",
     version="0.1.0",
     author="Rob Dalton",
     description="Package for creating ETL pipelines with Pandas DataFrames.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rob-dalton/pandas-pipeline",
+    url="https://github.com/rob-dalton/pandas-etl-pipeline",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
